fa6b4f2baa3954238e2b28136b00ab5d42032bbd
ranzo3 <ranzotaylor@hotmail.com>
2020-11-05 09:25:08 -0800

    Fix Typo and remove add tempfiles from post-refresh, it is best done just one time in config-clone
     Changes to be committed:
    	deleted:    DBTEMPLATE/post-refresh/sql/dxh_add_tempfiles.sql
    	modified:   DBTEMPLATE/post-refresh/sql/dxh_wrapper_script.sql

--- DBTEMPLATE/post-refresh/sql/dxh_wrapper_script.sql
+++ DBTEMPLATE/post-refresh/sql/dxh_wrapper_script.sql
@@ -1,9 +1,7 @@
 REM sample sql script
-REM PROMPT *** executing dxh_rest_user_password.sql ***
+REM PROMPT *** executing dxh_reset_user_password.sql ***
 REM @${SQLDIR}/dxh_reset_user_password.sql
 REM create user testuser1
 REM PROMPT *** executing dxh_custom_create_user.sql ***
 REM @${SQLDIR}/dxh_custom_create_user.sql
-REM PROMPT *** executing dxh_add_tempfiles.sql ***
-REM @${SQLDIR}/dxh_add_tempfiles.sql
 
