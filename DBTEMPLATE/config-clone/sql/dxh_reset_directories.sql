22b92da4193f76bf095779c68446d91a9df9850c
Ranzo J Taylor, III <36489926+Ranzo3@users.noreply.github.com>
2020-12-03 12:55:44 -0800

    Merge New Hooks from Tim Gorman and Ranzo (#5)
    * Make .sh scripts executable
    
    * Bugfix in dxh_dbclone, make sure both -s and -t are set
    
    * Add log directory to DBTEMPLATE
    
    * Bug Fix to spool to correct place (post-refresh/sql)
    
    	modified:   DBTEMPLATE/pre-refresh/sql/dxh_extract_user_password.sql
    
    * Improve extract password to use current technique for newer database versions
    
    	modified:   DBTEMPLATE/pre-refresh/sql/dxh_extract_user_password.sql
    
    * Ingtegrated some hook scripts from dlpx-ps-gate into this project
    
     Changes to be committed:
    	new file:   DBTEMPLATE/config-clone/scripts/add_oratab_tnsname.sh
    	modified:   DBTEMPLATE/config-clone/scripts/dxh_wrapper_script.sh
    	new file:   DBTEMPLATE/config-clone/scripts/ora_vdb_dblink_confclone.sh
    	new file:   DBTEMPLATE/config-clone/scripts/ora_vdb_masking_prep.sh
    	modified:   DBTEMPLATE/post-refresh/scripts/dxh_wrapper_script.sh
    	new file:   DBTEMPLATE/post-refresh/scripts/ora_vdb_dblink_confclone.sh
    	new file:   DBTEMPLATE/post-refresh/scripts/ora_vdb_masking_prep.sh
    
    * Improve description of add_oratab_tnsname.sh
    
    * Add tempfiles script to recreate tempfiles after cloning from Amazon RDS Oracle,
      which has DDL triggers causing the normal tempfile recreate to fail.
    
    * Improve wrapper scripts to refrence new scripts
    
    	new file:   DBTEMPLATE/config-clone/sql/dxh_add_tempfiles.sql
    	modified:   DBTEMPLATE/config-clone/sql/dxh_wrapper_script.sql
    	new file:   DBTEMPLATE/post-refresh/sql/dxh_add_tempfiles.sql
    	modified:   DBTEMPLATE/post-refresh/sql/dxh_wrapper_script.sql
    	modified:   DBTEMPLATE/pre-refresh/sql/dxh_wrapper_script.sql
    
    * Added a new config-clone hook to reset DBA_DIRECTORIES
    
    	new file:   DBTEMPLATE/config-clone/sql/dxh_reset_directories.sql
            modified:   DBTEMPLATE/config-clone/sql/dxh_wrapper_script.sql
    
    * Fix Typo and remove add tempfiles from post-refresh,
       it is best done just one time in config-clone
    
     Changes to be committed:
    	deleted:    DBTEMPLATE/post-refresh/sql/dxh_add_tempfiles.sql
    	modified:   DBTEMPLATE/post-refresh/sql/dxh_wrapper_script.sql

---
+++ DBTEMPLATE/config-clone/sql/dxh_reset_directories.sql
@@ -0,0 +1,3 @@
+REM sample script to reset dba_directories to new locations
+create or replace directory DATA_PUMP_DIR as '/home/delphix/datapump';
+
--- DBTEMPLATE/config-clone/sql/dxh_wrapper_script.sql
+++ DBTEMPLATE/config-clone/sql/dxh_wrapper_script.sql
@@ -4,3 +4,8 @@ REM @${SQLDIR}/dxh_reset_user_password.sql
 REM create user testuser1
 REM PROMPT *** executing dxh_custom_create_user.sql ***
 REM @${SQLDIR}/dxh_custom_create_user.sql
+REM PROMPT *** executing dxh_add_tempfiles.sql ***
+REM @${SQLDIR}/dxh_add_tempfiles.sql
+REM PROMPT *** executing dxh_reset_directories.sql ***
+REM @${SQLDIR}/dxh_reset_directories.sql
+
