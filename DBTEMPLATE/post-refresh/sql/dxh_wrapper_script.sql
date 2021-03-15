835c9db3e9def2f6605e26d2b18684be04bac19a
soodabhinav <78171874+soodabhinav@users.noreply.github.com>
2021-03-16 02:22:01 +0530

    Update dxh_wrapper_script.sql
    executing grants script

--- DBTEMPLATE/post-refresh/sql/dxh_wrapper_script.sql
+++ DBTEMPLATE/post-refresh/sql/dxh_wrapper_script.sql
@@ -4,6 +4,6 @@ REM @${SQLDIR}/dxh_reset_user_password.sql
 REM create user testuser1
 REM PROMPT *** executing dxh_custom_create_user.sql ***
 REM @${SQLDIR}/dxh_custom_create_user.sql
-REM PROMPT *** executing user_grants.sql ***
-REM @${SQLDIR}/user_grants.sql
+REM PROMPT *** executing dxh_extract_grants_users.sql ***
+REM @${SQLDIR}/dxh_extract_grants_users.sql
 
