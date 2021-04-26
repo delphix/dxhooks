fac1241c5de49aa6f654b77b726b662b5cb6e9d1
soodabhinav <78171874+soodabhinav@users.noreply.github.com>
2021-04-26 21:40:03 +0530

    Create extract_usersddl_privs.sh
    Adding script to create DDL and privs for users in multitanent database

---
+++ DBTEMPLATE/pre-refresh/scripts/extract_usersddl_privs.sh
@@ -0,0 +1,17 @@
+rm -f $SQLDIR/../../post-refresh/sql/dxh_set_grants_allusers_new.sql 
+{
+sqlplus -s "/ as sysdba"  <<EOF
+SET HEADING OFF;
+SET FEEDBACK OFF;
+SET LINESIZE 300;
+REM ALTER SESSION SET CONTAINER =$DELPHIX_PDB_NAME;
+ALTER SESSION SET CONTAINER =$1;
+SELECT username from dba_users where ORACLE_MAINTAINED='N' and username not like 'C##%' and username not like 'PDBADMIN'; 
+EOF
+} | while read line
+do
+  if [ "$line" ] # Line not NULL
+  then 
+sqlplus -s "/ as sysdba"  @$SQLDIR/dxh_extract_all_users.sql $line $1
+fi    
+done
